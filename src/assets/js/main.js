b5dd4045eb4d76e359372565bd75f7fd992632e7
Claudio d'Angelis <claudiodangelis@gmail.com>
2013-12-30 20:26:37 +0100

    Added search box. w00t!

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,7 @@
 $(window).load(function() {
-    
+
+
+
     var imgs = document.getElementsByTagName("img");
     for (var i = 0; i < imgs.length; i++) {
         if (imgs[i].id != "avatar") {
@@ -9,30 +11,41 @@ $(window).load(function() {
 
     $(document).keyup(function(e) {
         var url;
-        switch(e.keyCode) {
-            case 66:
-                // B
-                url = '/';
-                break;
-            case 80:
-                // P
-                url = '/projects/';
-                break;
-            case 65:
-                // A
-                url = '/about/';
-                break;
-            case 70:
-                // F
-                url = '/archive.html';
-                break;
-            case 222:
-                // ?
-                $('#shortcuts').modal();
-                break;                
-        }
-        if ( typeof url != 'undefined' && document.location.pathname.toString() != url ) {
-            location.href = url;
+        if ($('#searchbox').val() == '')
+        {
+            switch(e.keyCode) {
+                case 66:
+                    // B
+                    url = '/';
+                    break;
+                case 80:
+                    // P
+                    url = '/projects/';
+                    break;
+                case 65:
+                    // A
+                    url = '/about/';
+                    break;
+                case 70:
+                    // F
+                    url = '/archive.html';
+                    break;
+                case 222:
+                    // ?
+                    $('#shortcuts').modal();
+                    break;                
+            }
+            if ( typeof url != 'undefined' &&
+                document.location.pathname.toString() != url ) {
+                    location.href = url;
+            }
         }
     });
-})
+});
+
+function valid_search()
+{
+    var s = window.location.search;
+    if (s == '' || s == '?q=' || new RegExp(/\?[^q]/).test(s)) return false;
+    return true;
+}
